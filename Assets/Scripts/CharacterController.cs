04eca24ba670a38d3df6206a9def2e8e9cf3593b
Endercat9999 <2005martarosa@gmail.com>
2024-11-22 09:25:40 +0100

    move

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,95 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private CharacterController _controller;
+    private Transform _camera;
+    private Animator _animator;
+
+    private float _horizontal;
+    private float _vertical;
+    [SerializeField] private float _movementSpeed = 5;
+
+    private float _turnSmoothVelocity;
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+
+    [SerializeField] private float _jumpHeight = 1;
+
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
+
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] Transform _sensorPosition;
+    [SerializeField] float _sensorRadius = 0.5f;
+
+    
+    void Awake()
     {
-        
+        _controller = GetComponent<CharacterController>();
+        _camera = Camera.main.transform;
+        _animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Jump") && IsGrouded())
+        {
+            Jump();
+        }
+
+        Movement();
+       
+        Gravity();
     }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+
+        _animator.SetFloat("VelZ", direction.magnitude);
+        _animator.SetFloat("VelX", 0);
+
+        if(direction !=Vector3.zero)
+        {
+
+          float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+          float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+          transform.rotation = Quaternion.Euler(0, smoothAngle, 0);  
+
+           Vector3 moveDirecion = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+          _controller.Move(moveDirecion * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void Gravity()
+    {
+        if(!IsGrouded())
+        {
+           _playerGravity.y += _gravity * Time.deltaTime;
+        }
+        else if(IsGrouded() && _playerGravity.y < 0)
+        {
+            _animator.SetBool("IsJumpping", false);
+
+            _playerGravity.y = -1;
+        }
+        _controller.Move(_playerGravity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        _animator.SetBool("IsJumpping", true);
+        _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+    }
+
+    bool IsGrouded()
+    {
+        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+    }
+
+
 }
